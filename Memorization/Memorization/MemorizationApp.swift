fba8c6f984b6f2e042c8776e7116ec7d319b147e
wzhang68 <wzhang68@usc.edu>
2021-07-13 00:43:23 -0700

    init July 13

---
+++ Memorization/Memorization/MemorizationApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizationApp.swift
+//  Memorization
+//
+//  Created by WEI ZHANG on 7/13/21.
+//
+
+import SwiftUI
+
+@main
+struct MemorizationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
